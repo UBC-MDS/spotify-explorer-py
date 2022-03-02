5ef8950ee9a3d2096fa9fce91efb7f7e664dec28
christopheralex <christopher.a0@gmail.com>
2022-03-02 01:38:49 -0800

    feat: Adds initial layout for all plots

--- src/layout_components.py
+++ src/layout_components.py
@@ -13,31 +13,137 @@ formatted_date = today.strftime("%b %d, %Y")
 
 SPOTIFY_LOGO = "assets/img/spotify-ex.png"
 
+
 def get_artist_section():
-    '''
-    2 columns 
-    1col - Widgets 
-    2col - with 2 rows where first row is 1 column and other row is 2 columns 
-    '''
-    pass
-    # section = html.Div(
-    #     [
-    #         dbc.Row(
-    #             [
+    """
+    1 row
+    2 columns
+    1col - Widgets
+    2col - with 2 rows where first row is 1 column and other row is 2 columns
+    """
 
-    #             ]
-    #         )
-    #     ]
+    sidebar_widegets = dbc.Col(
+        children=[
+            html.H1("Sidebar widgets", className="display-30"),
+            html.Iframe(
+                id="widget_id",
+                style={
+                    "width": "20%",
+                    "height": "100px",
+                    "border": "0px",
+                },
+            ),
+        ]
+    )
 
-    # )
+    plot_1_settings = dbc.Col(
+        [
+            html.H1("Plot 1 with Artist/Genre", className="display-30"),
+            # Pseudo code for plot specfication, pleae modify
+            html.Iframe(
+                id="artist_genre_bar_id",
+                style={
+                    "width": "100%",
+                    "height": "350px",
+                    "border": "0px",
+                },
+            ),
+        ]
+    )
+
+    plot_2_settings = dbc.Col(
+        [
+            html.H1("Plot 2 with Artist trend", className="display-30"),
+            # Pseudo code for plot specfication, pleae modify
+            html.Iframe(
+                id="artist_trend_id",
+                style={
+                    "width": "50%",
+                    "height": "350px",
+                    "border": "0px",
+                },
+            ),
+        ]
+    )
+
+    plot_3_settings = dbc.Col(
+        [
+            html.H1("Plot 3 with Artist pop histogram", className="display-30"),
+            # Pseudo code for plot specfication, pleae modify
+            html.Iframe(
+                id="artist_pop_hist_id",
+                style={
+                    "width": "50%",
+                    "height": "350px",
+                    "border": "0px",
+                },
+            ),
+        ]
+    )
+
+    section = html.Div(
+        [
+            dbc.Row(
+                children=[
+                    sidebar_widegets,
+                    dbc.Col(
+                        children=[
+                            dbc.Row(children=[plot_1_settings]),
+                            dbc.Row(
+                                children=[
+                                    plot_2_settings,
+                                    plot_3_settings,
+                                ]
+                            ),
+                        ]
+                    ),
+                ]
+            )
+        ]
+    )
+
+    return section
+
 
 def get_popularity_section():
-    '''
-    2 columns 
-    1col - Widgets 
-    2col - with 2 rows where first row is 1 column and other row is 2 columns 
-    '''
-    pass
+    """
+    1 row
+    2 columns
+    1col - Widgets
+    2col - Plot
+    """
+    sidebar_widegets = dbc.Col(
+        children=[
+            html.H1("Sidebar widgets", className="display-30"),
+            html.Iframe(
+                id="widget_id2",
+                style={
+                    "width": "20%",
+                    "height": "100px",
+                    "border": "0px",
+                },
+            ),
+        ]
+    )
+
+    plot_4_settings = dbc.Col(
+        [
+            html.H1("Plot 4 for pop/unpop", className="display-30"),
+            # Pseudo code for plot specfication, pleae modify
+            html.Iframe(
+                id="pop_unpop_id",
+                style={
+                    "width": "100%",
+                    "height": "350px",
+                    "border": "0px",
+                },
+            ),
+        ]
+    )
+
+    section = html.Div([dbc.Row(children=[sidebar_widegets, plot_4_settings])])
+
+    return section
 
 
 def get_tab_section():
@@ -60,17 +166,16 @@ def get_tab_section():
 navbar = dbc.Navbar(
     dbc.Container(
         [
-             html.A(
+            html.A(
                 dbc.Row(
                     [
                         dbc.Col(html.Img(src=SPOTIFY_LOGO, height="70px")),
-                        dbc.Col(dbc.NavbarBrand("Spotify Explorer",
-                        className="py-10")),
+                        dbc.Col(dbc.NavbarBrand("Spotify Explorer", className="py-10")),
                     ],
                     align="center",
                     className="g-0",
                 )
-             )
+            )
         ]
     )
 )
@@ -80,8 +185,10 @@ container = dbc.Container(
         html.Br(),
         get_tab_section(),
         html.Footer(
-            [f"(C) Copyright MIT License: Christopher Alexander, Jennifer Hoang, Michelle, Thea Wenxin. ",
-             f"Last time updated on {formatted_date}."],
+            [
+                f"(C) Copyright MIT License: Christopher Alexander, Jennifer Hoang, Michelle, Thea Wenxin. ",
+                f"Last time updated on {formatted_date}.",
+            ],
             # style=FOOTER_STYLE,
         ),
     ]
