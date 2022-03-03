a3db1447f63d5de3f30c0c7158647276dd2c8e10
michelle-wms <meisi.wg@gmail.com>
2022-03-03 00:02:58 -0800

    Layout changes and added plot2

--- src/app.py
+++ src/app.py
@@ -10,27 +10,11 @@ import layout_components as lc
 
 alt.data_transformers.disable_max_rows()
 
-# Intial read data for prototype
-df = pd.read_csv("data/raw/spotify.csv")
-# print(df.head())
+# Read raw data
+df = pd.read_csv("../data/raw/spotify.csv", parse_dates=['track_album_release_date'])
+df.dropna(inplace=True)
 
-
-def artist_popularity_hist():
-    pass
-
-
-def top_artists():
-    pass
-
-
-def artist_popularity_trend():
-    pass
-
-
-def popular_non_popular_line():
-    pass
-
-
+# Set up app frontend
 app = Dash(
     __name__,
     title="Spotify Explorer",
@@ -38,10 +22,12 @@ app = Dash(
     external_stylesheets=[dbc.themes.MINTY],
     suppress_callback_exceptions=True,
 )
+
 server = app.server
 
 app.layout = html.Div([lc.navbar, lc.container], style={"backgroundColor": "#eeeeef"})
 
+# Tabs ----------
 
 @app.callback(Output("tab-content", "children"), [Input("tabs", "active_tab")])
 def switch_tab(tab_id):
@@ -50,6 +36,48 @@ def switch_tab(tab_id):
     elif tab_id == "tab-2":
         return lc.get_popularity_section()
     return html.P("This shouldn't ever be displayed...")
+
+# Plots -------------
+
+## Plot1
+def top_artists():
+    pass
+
+## Plot2
+@app.callback(
+    Output('artist_trend_plot', 'srcDoc'),
+    Input('artist_selection', 'value')
+)
+
+def artist_trend_plot(track_artist = 'Ed Sheeran'):
+    
+    trend_data = df.query("track_artist == 'Ed Sheeran' ")
+
+    if track_artist != "Ed Sheeran":
+        trend_data = df.query("track_artist == @track_artist")
+        
+    chart = alt.Chart(trend_data).mark_line().encode(
+        alt.X('track_album_release_date', axis=alt.Axis(title="Album release date")),
+        alt.Y('mean(track_popularity)', axis=alt.Axis(title="Popularity")))\
+            .properties(height=250, width=350, background='#eeeeef')
+        #tooltip=track_artist).interactive()
+    return chart.to_html()
+
+## Plot3
+def artist_popularity_hist():
+    pass
+
+## Plot4
+def popular_non_popular_line():
+    pass
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
